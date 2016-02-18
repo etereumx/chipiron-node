f3bab408213f859e18eea97ddb08628123fd86bb
etereumx <oscarha@gmail.com>
2016-02-18 18:30:00 +0100

    firt version

--- robot/salamardigras.js
+++ robot/salamardigras.js
@@ -1,6 +1,15 @@
 /**
  * http://usejsdoc.org/
  */
+var http = require('http');
 var urlSource= "http://www.salamardigras.com/newmardi/category/conciertos/";
+var request = http.get(urlSource, function(res){
+	res.setEncoding('utf8');
+	res.on('data', function (chunk) {
+	    console.log('BODY: ' + chunk);
+	  });
+});
 
-console.log("sala mardigras");
+request.on('error', function(e){
+	console.log("error:"+e);
+});
